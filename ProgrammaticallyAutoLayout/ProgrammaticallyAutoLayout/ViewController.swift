cf2071c8a22ba573790d4b01d12ef5d29ee1739d
Jay Ramirez <ramirezblancasjuanm@gmail.com>
2024-04-20 23:42:57 -0600

    Working on more auto layout

---
+++ ProgrammaticallyAutoLayout/ProgrammaticallyAutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProgrammaticallyAutoLayout
+//
+//  Created by Juan Ramírez Blancas on 20/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

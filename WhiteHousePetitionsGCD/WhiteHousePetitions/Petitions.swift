77c922097c55b55f4208734c6bc41b7fd4b8bfe3
Jay Ramirez <ramirezblancasjuanm@gmail.com>
2024-04-24 11:34:15 -0600

    Working with Grand Central Distaptch on Whitehouse petitions project

---
+++ WhiteHousePetitionsGCD/WhiteHousePetitions/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  WhiteHousePetitions
+//
+//  Created by Juan Ramírez Blancas on 22/04/24.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}

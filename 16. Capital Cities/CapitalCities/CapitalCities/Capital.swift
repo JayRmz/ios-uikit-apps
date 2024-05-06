c5c02472179ccf7f6d1a011687aa48492566ca98
Jay Ramirez <ramirezblancasjuanm@gmail.com>
2024-05-06 12:25:56 -0600

    Capital cities

---
+++ 16. Capital Cities/CapitalCities/CapitalCities/Capital.swift
@@ -0,0 +1,21 @@
+//
+//  Capital.swift
+//  CapitalCities
+//
+//  Created by Juan Ramírez Blancas on 06/05/24.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String? = nil, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
